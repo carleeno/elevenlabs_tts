34ccd1af7c237d19de89380c47e54b59a1d86590
mete0r <50095511+lbwniubi777@users.noreply.github.com>
2023-10-11 23:13:11 +0800

    Update elevenlabs.py

--- custom_components/elevenlabs_tts/elevenlabs.py
+++ custom_components/elevenlabs_tts/elevenlabs.py
@@ -19,9 +19,9 @@ from .const import (
     DEFAULT_OPTIMIZE_LATENCY,
     DEFAULT_SIMILARITY,
     DEFAULT_STABILITY,
-    DEFAULT_VOICE,
     DEFAULT_STYLE,
     DEFAULT_USE_SPEAKER_BOOST,
+    DEFAULT_VOICE,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -120,16 +120,10 @@ class ElevenLabsClient:
         self, message: str, options: dict | None = None
     ) -> tuple[str, bytes]:
         """Get text-to-speech audio for the given message."""
-        (
-            voice_id,
-            stability,
-            similarity,
-            model,
-            optimize_latency,
-            api_key,
-            style,
-            use_speaker_boost,
-        ) = await self.get_tts_options(options)
+        tts_options = await self.get_tts_options(options)
+        voice_id, stability, similarity, model, optimize_latency, api_key = tts_options[
+            :6
+        ]
 
         endpoint = f"text-to-speech/{voice_id}"
         data = {
@@ -140,9 +134,12 @@ class ElevenLabsClient:
                 "similarity_boost": similarity,
             },
         }
-        if model == 'eleven_multilingual_v2':
+
+        if model == "eleven_multilingual_v2":
+            style, use_speaker_boost = tts_options[6:]
             data["voice_settings"]["style"] = style
             data["voice_settings"]["use_speaker_boost"] = use_speaker_boost
+
         params = {"optimize_streaming_latency": optimize_latency}
         _LOGGER.debug("Requesting TTS from %s", endpoint)
         _LOGGER.debug("Request data: %s", data)
@@ -153,7 +150,7 @@ class ElevenLabsClient:
 
     async def get_tts_options(
         self, options: dict
-    ) -> tuple[str, float, float, str, int, str]:
+    ) -> tuple[str, float, float, str, int, str, float, bool]:
         """Get the text-to-speech options for generating TTS audio."""
         # If options is None, assign an empty dictionary to options
         if not options:
@@ -200,19 +197,7 @@ class ElevenLabsClient:
             or self.config_entry.options.get(CONF_API_KEY)
             or self._api_key
         )
-        
-        style = (
-            options.get(CONF_STYLE)
-            or self.config_entry.options.get(CONF_STYLE)
-            or DEFAULT_STYLE
-        )
-        
-        use_speaker_boost = (
-            options.get(CONF_USE_SPEAKER_BOOST)
-            or self.config_entry.options.get(CONF_USE_SPEAKER_BOOST)
-            or DEFAULT_USE_SPEAKER_BOOST
-        )
-        
+
         # Convert optimize_latency to an integer
         optimize_latency = int(optimize_latency)
 
@@ -238,4 +223,33 @@ class ElevenLabsClient:
                 )
                 voice_id = self._voices[0]["voice_id"]
 
-        return voice_id, stability, similarity, model, optimize_latency, api_key, style, use_speaker_boost,
+        if model == "eleven_multilingual_v2":
+            style = (
+                options.get(CONF_STYLE)
+                or self.config_entry.options.get(CONF_STYLE)
+                or DEFAULT_STYLE
+            )
+            use_speaker_boost = (
+                options.get(CONF_USE_SPEAKER_BOOST)
+                or self.config_entry.options.get(CONF_USE_SPEAKER_BOOST)
+                or DEFAULT_USE_SPEAKER_BOOST
+            )
+            return (
+                voice_id,
+                stability,
+                similarity,
+                model,
+                optimize_latency,
+                api_key,
+                style,
+                use_speaker_boost,
+            )
+
+        return (
+            voice_id,
+            stability,
+            similarity,
+            model,
+            optimize_latency,
+            api_key,
+        )
